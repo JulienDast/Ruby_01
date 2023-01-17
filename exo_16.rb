64b09e6684a1ba662c06038107ea3439d486253b
JulienDast <julien.dast44@gmail.com>
2023-01-17 22:49:47 +0100

    finitions

--- exo_16.rb
+++ exo_16.rb
@@ -1,6 +1,12 @@
 puts "Salut, bienvenue dans l'exceptionnelle pyramide ! Combien d'étages souhaites-tu ?"
 print ">"
 number = gets.chomp.to_i
+i=1
+j= number-1
+bloc="#"
+space = " "
+
+
 if number > 25
   puts "Désolé mon ami, c'est trop... Moins de 25 !"
 elsif number <=0
@@ -8,20 +14,14 @@ elsif number <=0
 end
 
 if number <= 25 && number >1
-i = 0
-space = " "  
-bloc = "#"
-number.times do
-  if number>=0 && i<= number
-  number.times do
-  print "#{space}"
-  number-1
-  end
-  puts "#{bloc}"  
-  i+1
-  bloc = bloc + "#"
+  for num in 1..number do
+    (number-num).times {print ' '}
+    num.times{print "#"}
+    puts
   end
 end
-end
+
+
+
 
 
